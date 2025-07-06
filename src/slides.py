0f381e3ec98d02e93373289a236425a984bd6ef2
Vetle <vetle@tjora.net>
2025-07-06 22:35:21 +0200

    docs: restructure and add additional comments

--- src/slides.py
+++ src/slides.py
@@ -6,28 +6,52 @@ import yaml
 import argparse
 from collections.abc import Iterable
 
-# parse potential arguments: 
+# parse arguments: 
 parser = argparse.ArgumentParser(
         description="Create a text-based presentation."
     )
-parser.add_argument("-b", "--bulletpoint", help="Alter bulletpoints")
-parser.add_argument("-n", "--nobulletpoints", action="store_true", default=False)
-parser.add_argument("-p", "--pages", nargs="+", help="Pages to include")
-parser.add_argument("-c", "--colour", "--color", help="Choose predefined colour scheme")
-parser.add_argument("--notitle", action="store_true", default=False, help="Toggle off title slide")
-parser.add_argument("content", help=".yaml file with slide content")
+
+parser.add_argument(
+        "-b", "--bulletpoint",
+        help="Alter bulletpoints"
+    )
+parser.add_argument(
+        "-n", "--nobulletpoints",
+        action="store_true",
+        default=False
+    )
+parser.add_argument(
+        "-p", "--pages", 
+        nargs="+", 
+        help="Pages to include"
+    )
+parser.add_argument(
+        "-c", "--colour", "--color",
+        help="Choose predefined colour scheme"
+    )
+parser.add_argument(
+        "--notitle",
+        action="store_true",
+        default=False,
+        help="Toggle off title slide"
+    )
+parser.add_argument(
+        "content",
+        help=".yaml file with slide content")
 
 args = parser.parse_args()
 
 notitle = args.notitle
 
+# load the title
 with open(args.content) as file:
     title = yaml.safe_load(file)['title']
 
-# read configurations:
-with open("style/config.yaml") as file:
+# load style
+with open('style/config.yaml') as file:
     configs = yaml.safe_load(file)
 
+# set colour scheme if -c flag is included
 if args.colour:
     match args.colour.lower():
         case "b" | "blue":
@@ -46,9 +70,11 @@ if args.colour:
     with open("style/colour-schemes.yaml") as file:
         colours = yaml.safe_load(file)[colour]
 else:
+    # otherwise use the colours from the config file
     colours = configs["colours"]
 
-# set bulletpoint character. -b flag overrides config.yaml
+# set bulletpoint character
+# -b flag overrides config.yaml
 if not args.nobulletpoints: # -n flag overrides -b flag and config.yaml
     if args.bulletpoint is None:
         bulletpoint = configs["bulletpoint"]
@@ -56,8 +82,9 @@ if not args.nobulletpoints: # -n flag overrides -b flag and config.yaml
         bulletpoint = args.bulletpoint
     # make sure bulletpoints are of length 1
     if len(bulletpoint) != 1:
-        raise ValueError("Bullet point must be single character (see config.yaml)")
+        raise ValueError("Bullet point must be single character")
 
+# make sure -p arguments are integers:
 if not args.pages is None:
     if isinstance(args.pages, Iterable):
         try:
@@ -70,7 +97,7 @@ if not args.pages is None:
         except ValueError:
             raise ValueError("--pages requires integers")
 
-# set colours from config.yaml:
+# set colours:
 t_bg = colours["title_bkgd"] # title background colour
 b_bg = colours["text_bkgd"] # main body background colour
 t_txt = colours["title_colr"] # title text colour
@@ -112,9 +139,19 @@ def create_slide(stdscr, title, text, title_slide=False):
     
     # add underlined and ALL-CAPS title
     if title_slide:
-        titlewin.addstr(height//10, title_coor, title.upper(), curses.A_UNDERLINE)
+        titlewin.addstr(
+                height//10,
+                title_coor,
+                title.upper(),
+                curses.A_UNDERLINE
+            )
     else:
-        titlewin.addstr(1, title_coor, title.upper(), curses.A_UNDERLINE)
+        titlewin.addstr(
+                1,
+                title_coor,
+                title.upper(),
+                curses.A_UNDERLINE
+            )
 
     if not title_slide:
         # add bullet points to paragraphs
@@ -133,12 +170,13 @@ def create_slide(stdscr, title, text, title_slide=False):
             if args.nobulletpoints:
                 indent = ''
             else:
-                if line == '':
-                    indent = ''
-                elif line[0] == '\u2022':
-                    indent = ''
+                if line == '': 
+                    indent = '' # no indent on empty lines
+                elif line[0] == '\u2022':  
+                    indent = '' # no indent on bullet points
                 else:
-                    indent = '  ' # indent lines without bullet points to align well
+                    indent = '  ' # indent lines without bullet points
+
             textwin.addstr(row, 0, f'{indent}{line}') # add line to window
 
     # refresh windows
@@ -149,7 +187,6 @@ def create_slide(stdscr, title, text, title_slide=False):
     while True:
         key = stdscr.getch() # wait for input
 
-
         # if input is <enter> or <right>, output "next"
         if key in (curses.KEY_RIGHT, 32, curses.KEY_ENTER, 10, 13):
             return "next"
@@ -158,7 +195,7 @@ def create_slide(stdscr, title, text, title_slide=False):
         elif key == curses.KEY_LEFT:
             return "prev"
 
-        # quit on Q
+        # quit on capital Q
         elif key == 81:
             exit()
 
@@ -195,18 +232,29 @@ def main(stdscr):
     # load slide content from content.yaml
     with open(args.content) as file:
         content = yaml.safe_load(file)
+
+    # amount of slides
     l = len(content['slides'])
+
+    # create index lists of page numbers, e.g.:
+    # ["t", 1, 2, ..., N] for all N slides including title screen
+    # [1, 2, ..., N] for all N slides without title screen
+    # [3, 5, 2, "t"] for 3rd, 5th, 2nd and title slides in that order
     try:
         # check for user specified page numbers
         pages = globals()["pages"]
         pages = ["t" if p == 0 else p-1 for p in pages] # reindex
+        
+        # make sure they're within the range
         if not all(0 <= p < l if isinstance(p, int) else True for p in pages):
             raise IndexError("Page numbers out of range")
     except KeyError:
         # else set all pages and alternatively title 
         pages = list(range(l))
+
+        # add title unless explicitly or implicitly stated otherwise
         if not globals()['notitle'] and globals()['title'] != '':
-            pages = ['t'] + pages # add title "index" if appropriate
+            pages = ['t'] + pages 
 
     i = 0 # begin indexing at 0
     while True:
